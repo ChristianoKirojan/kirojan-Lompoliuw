478df19fedb838343b8cf50e44bbcf7e7a309a96
ChristianoKirojan <s22310497@student.unklab.ac.id>
2025-11-04 15:06:41 +0800

    create tailwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-600"> Lav & Traff </h1>
+    </div>
+  )
+}
+
+export default App
